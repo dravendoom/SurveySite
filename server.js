3b5b4f02078d425cc8e9ae7d0665fb3c01153a28
dravendoom <dravendoom@gmail.com>
2022-10-26 11:58:42 -0400

    Server.js Uploaded manually
    Was not working with VScodes commit and push method for some reason, for now I am adding this manually.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+let port = 8080;
+let hostName = "localhost"
+
+let actualRootDirectory = "../../../client/layout"
+let aliasRootDirectory = "/main"
+
+app.use(aliasRootDirectory, express.static(actualRootDirectory));
+
+app.listen(port, hostName, ()=>{
+   console.log("Server started listening for connections");
+});
